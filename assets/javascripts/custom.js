2879d9a811b6b7589d76e6238a653f5a64a0ff9f
James Woglom <j@wogloms.net>
2019-08-23 14:00:20 -0400

    pw reset msg

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -23,6 +23,14 @@ var init = function() {
         c.setAttribute('style', 'margin-top: 20px');
         c.innerHTML = "<b>Applications are now open for HackUMass VII!</b><br />If you have previously created a Dashboard account then you'll need to register again. We can't wait for you to apply!";
         document.querySelector('.container').prepend(c);
+    } else if (location.pathname == '/users/password/new' && document.getElementById('apps-open') == null) {
+        var c = document.createElement('div');
+        c.setAttribute('id', 'apps-open');
+        c.classList.add('alert');
+        c.classList.add('alert-info');
+        c.setAttribute('style', 'margin-top: 20px');
+        c.innerHTML = "<b>If you have previously created a Dashboard account then you'll need to register again instead of resetting your old password. Thanks!</b>";
+        document.querySelector('.container').prepend(c);
     }
 }
 
